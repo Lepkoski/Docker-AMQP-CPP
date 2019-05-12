930f14d42c9c5fdb87e5ec3f0f54464d9069bcd5
Rafael Lepkoski <lepkoski@gmail.com>
2019-05-13 08:35:39 +1200

    Configuring Dockerfile

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Docker - AMQP-CPP
 
 Docker image for AMQP-CPP
+
+Use `build.sh` to build all necessary images
---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker build . -f Dockerfile-requirements -t amqp-cpp-requirements
+docker build . -f Dockerfile-source -t amqp-cpp-source
+docker build . -f Dockerfile-make -t amqp-cpp-make
+# docker build . -f Dockerfile -t amqp-cppw
